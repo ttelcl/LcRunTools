86e54974d6362b96122105add9433360c83fe300
Luc Cluitmans <9429819+ttelcl@users.noreply.github.com>
2022-05-01 22:32:32 +0300

    Attempt to set the assembly version.
    The approach works, but uses a deprecated mechanism. About to back out...

---
+++ lcrun/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿module AssemblyInfo
+
+open System.Reflection
+
+(*
+ ALERT:
+ For these attributes to have any effect, the project file must contain:
+
+   <PropertyGroup>
+     <GenerateAssemblyInfo>false</GenerateAssemblyInfo>
+   </PropertyGroup>
+
+*)
+
+[<assembly: AssemblyVersion("1.0.1.0")>]
+[<assembly: AssemblyFileVersion("1.0.1.0")>]
+[<assembly: AssemblyDescription("lcrun - application bootstrap utility")>]
+[<assembly: AssemblyTitle("lcrun - application bootstrap utility")>]
+
+do()
